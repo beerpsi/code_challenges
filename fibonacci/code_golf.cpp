989316008afb3e2d85fd9ce51498a163aaf0d49f
beerpsi <vaythuonggialac@tutanota.com>
2020-07-23 10:19:27 +0700

    Slim down the submitting solution even more

--- fibonacci/code_golf.cpp
+++ fibonacci/code_golf.cpp
@@ -1,10 +1,2 @@
-#include <stdio.h>
-int main(){
-long double i,l=0,c=1; 
-printf("%.0Lf\n",l);
-printf("%.0Lf\n",c); 
-for (i=0;i<98;i++){
-printf("%.0Lf\n",c=l+c);
-l=c-l;
-}
-} 
+#include <stdio.h> 
+int main(){long double i,l=0,c=1;printf("%.0Lf\n",l);printf("%.0Lf\n",c);for(i=0;i<98;i++){printf("%.0Lf\n",c=l+c);l=c-l;}} 
