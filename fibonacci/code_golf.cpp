616fff0191831ad1800f54278b9cb3f5741e45ba
beerpsi <vaythuonggialac@tutanota.com>
2020-07-23 10:14:54 +0700

    Slim down on the submitting solution, make another one that looks good

--- fibonacci/code_golf.cpp
+++ fibonacci/code_golf.cpp
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 int main(){
-    long double next_number, last_number = 0, curr_number = 1;
-    printf("%-5.0Lf\n", last_number);
-    printf("%-5.0Lf\n", curr_number); 
-    for (int i = 0; i < 98; i++){
-        next_number = last_number + curr_number;
-        printf("%-5.0Lf\n", next_number);
-        last_number = curr_number;
-        curr_number = next_number;
-    }
+long double i,l=0,c=1; 
+printf("%.0Lf\n",l);
+printf("%.0Lf\n",c); 
+for (i=0;i<98;i++){
+printf("%.0Lf\n",c=l+c);
+l=c-l;
+}
 } 
